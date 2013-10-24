6193af48379652e3a955e147fb5bae1ed20e5889
Fsomogyi <ferencsomogyi92@gmail.com>
2013-10-24 22:18:51 +0200

    Céges táblák berakva, beszúrás cégek közé lehetséges.

--- crmpp/src/crmpp/Application.java
+++ crmpp/src/crmpp/Application.java
@@ -1,6 +1,7 @@
 package crmpp;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Application {
 
@@ -8,11 +9,15 @@ public class Application {
 		
 		Model model = new Model();
 		
-		model.createUserTables();
+		model.createTables();
 		
-		model.insertUser(1, "Teszt", "Elek", "M", 66, "tesztelek@ittvagyok.hu", "golf");	
+		model.insertUser(1, "Teszt", "Elek", "M", 66, "tesztelek@ittvagyok.hu", "golf", "");
+		model.insertCorporation(1, "Teszt Rt.", "999 Valahol Valamilyen utca 3.", "tesztrt@ezegyceg.hu");
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		//list.add(999);
+		
+		model.addCorporationContacts(1, list);
 	}
-	
-	
-
 }
--- crmpp/src/crmpp/Model.java
+++ crmpp/src/crmpp/Model.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Model {
 
@@ -24,21 +25,24 @@ public class Model {
 		}
 	}
 	
-	// Creates the tables that store user data; drops existing ones
-	public void createUserTables() throws SQLException {
+	// Creates the tables that store user and corporation data; drops existing ones
+	public void createTables() throws SQLException {
 		if (conn != null)
 		{
 			java.sql.Statement stmt = conn.createStatement();
 			
 			String dropString = "";
 			
+			dropString = "DROP TABLE IF EXISTS crmcorporationuserconnection";
+			stmt.execute(dropString);
+			dropString = "DROP TABLE IF EXISTS crmcorporations";
+			stmt.execute(dropString);
 			dropString = "DROP TABLE IF EXISTS crmavailabilities";
 			stmt.execute(dropString);
 			dropString = "DROP TABLE IF EXISTS crminterests";
 			stmt.execute(dropString);
 			dropString = "DROP TABLE IF EXISTS crmusers";
 			stmt.execute(dropString);
-
 			
 			String createString = "CREATE TABLE crmusers (USER_ID INTEGER PRIMARY KEY, FIRST_NAME CHAR(50)," +
 									"LAST_NAME CHAR(75), GENDER CHAR(1), AGE INTEGER)";
@@ -46,26 +50,35 @@ public class Model {
 			
 			
 			createString = "CREATE TABLE crmavailabilities (USER_ID INTEGER not null, FOREIGN KEY (USER_ID) REFERENCES crmusers (USER_ID)"
-					+ ", EMAIL CHAR(50))";
+					+ ", EMAIL CHAR(50), ADDRESS CHAR(75))";
 			stmt.execute(createString);
 			
 
 			createString = "CREATE TABLE crminterests (USER_ID INTEGER not null, FOREIGN KEY (USER_ID) REFERENCES crmusers (USER_ID)"
 					+ ", FAVORITE_SPORT CHAR(50))";
 			stmt.execute(createString);
+			
+			createString = "CREATE TABLE crmcorporations (ID INTEGER PRIMARY KEY, NAME CHAR(50)," +
+					"ADDRESS CHAR(75), EMAIL CHAR(50))";
+			stmt.execute(createString);
+
+
+			createString = "CREATE TABLE crmcorporationuserconnection (USER_ID INTEGER not null, " +
+					"FOREIGN KEY (USER_ID) REFERENCES crmusers (USER_ID)"
+					+ ", CORPORATION_ID INTEGER not null, FOREIGN KEY (CORPORATION_ID) REFERENCES crmcorporations (ID))";
+			stmt.execute(createString);
 		}
 	}
 	
 	// Inserts a user into the database with the given parameters
 	public void insertUser(int userID, String firstName, String lastName, String gender, int age,
-					String email, String sport) throws SQLException {
+					String email, String address, String sport) throws SQLException {
 		if (conn != null)
 		{
-			// TODO: egzaktabb, pontosabb hibaüzenet
 			if (firstName.length() > 50 || lastName.length() > 75 || (!gender.equals("M") && !gender.equals("F"))
 					|| email.length() > 50 || sport.length() > 50)
 			{
-				System.out.println("Invalid argument(s)!");
+				System.out.println("Invalid argument(s) when inserting a user!");
 				return;
 			}
 			
@@ -82,10 +95,11 @@ public class Model {
 			
 			stmt.execute();
 			
-			query = "INSERT INTO crmavailabilities VALUES (?, ?)";
+			query = "INSERT INTO crmavailabilities VALUES (?, ?, ?)";
 			stmt = conn.prepareStatement(query);
 			stmt.setInt(1, userID);
 			stmt.setString(2, email);
+			stmt.setString(3, address);
 			stmt.execute();
 			
 			query = "INSERT INTO crminterests VALUES (?, ?)";
@@ -95,7 +109,7 @@ public class Model {
 			stmt.execute();
 			
 			//****************************************//
-			// Code from now on is used for testing only
+			// Run queries to check the inserted values
 			//****************************************//
 			
 			stmt = conn.prepareStatement("SELECT * FROM crmusers");
@@ -136,10 +150,86 @@ public class Model {
 					System.out.println(rm.getColumnName(i) + "\t" + rs.getString(i));
 				}
 			}
-			
-			//****************************************//
-			//************End of testing***************//
-			//****************************************//
 		}
-	}
+		}
+		
+		// Inserts a corporation into the database with the given parameters
+		public void insertCorporation(int ID, String name, String address, String email) throws SQLException {
+			if (conn != null)
+			{
+				if (name.length() > 50 || address.length() > 75 || email.length() > 50)
+				{
+					System.out.println("Invalid argument(s) when inserting a corporation!");
+					return;
+				}
+				
+				String query = "";
+				PreparedStatement stmt = null;
+				
+				query = "INSERT INTO crmcorporations VALUES (?, ?, ?, ?)";
+				stmt = conn.prepareStatement(query);
+				stmt.setInt(1, ID);
+				stmt.setString(2, name);
+				stmt.setString(3, address);
+				stmt.setString(4, email);
+				
+				stmt.execute();
+				
+				//****************************************//
+				// Run a query to check the inserted values
+				//****************************************//
+				
+				stmt = conn.prepareStatement("SELECT * FROM crmcorporations");
+				
+			    ResultSet rs = stmt.executeQuery();
+			      
+			    ResultSetMetaData rm = rs.getMetaData();
+
+				while (rs.next()) {
+					for (int i=1; i<5; i++)
+					{
+						System.out.println(rm.getColumnName(i) + "\t" + rs.getString(i));
+					}
+				}
+			}
+		}
+		
+		// Connects a corporation with the specified users
+		public void addCorporationContacts(int corporationID, ArrayList<Integer> users) throws SQLException {
+			if (conn != null)
+			{
+				// First check if the given IDs are valid, and if not, do not do anything
+				String query = "";
+				PreparedStatement stmt = null;
+				ResultSet rs = null;
+				
+				for(int userID : users)
+				{
+					query = "INSERT INTO crmcorporationuserconnection VALUES (?, ?)";
+					stmt = conn.prepareStatement(query);
+					stmt.setInt(1, corporationID);
+					stmt.setInt(2, userID);
+				}
+
+				
+				stmt.execute();
+				
+				//****************************************//
+				// Run a query to check the inserted values
+				//****************************************//
+				
+				stmt = conn.prepareStatement("SELECT * FROM crmcorporationuserconnection");
+				
+			    rs = stmt.executeQuery();
+			      
+			    ResultSetMetaData rm = rs.getMetaData();
+
+				while (rs.next()) {
+					for (int i=1; i<3; i++)
+					{
+						System.out.println(rm.getColumnName(i) + "\t" + rs.getString(i));
+					}
+				}
+			}
+		}
 }
