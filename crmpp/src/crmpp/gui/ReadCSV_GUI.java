a071cd6f908987d17e53e6574999c5f0b4b2c6f7
Fsomogyi <ferencsomogyi92@gmail.com>
2013-11-06 19:11:33 +0100

    GUI most már képes cégeket is kezelni

--- crmpp/src/crmpp/gui/ReadCSV_GUI.java
+++ crmpp/src/crmpp/gui/ReadCSV_GUI.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.border.EmptyBorder;
@@ -26,25 +27,48 @@ import crmpp.Application;
 public class ReadCSV_GUI extends JFrame{
 	private Application app;			// the application logic
 	
+	// user labels
 	private JLabel usersLabel;
 	private JLabel interestsLabel;
 	private JLabel availabilitiesLabel;
 	
+	// corporation labels
+	private JLabel corporationsLabel;
+	private JLabel corporationUserConnectionLabel;
+	
+	// user text fields
 	private JTextField usersTextField;
 	private JTextField interestsTextField;
 	private JTextField availabilitiesTextField;
 	
+	// corporation text fields
+	private JTextField corporationsTextField;
+	private JTextField corporationUserConnectionTextField;
+	
+	// user buttons
 	private JButton usersButton;
 	private JButton interestsButton;
 	private JButton availabilitiesButton;
-	
-	
 	private JButton okButton;
 	
-	private final JPanel groupPanel;
-	private final JPanel flowPanel;
+	// corporation buttons
+	private JButton corporationsButton;
+	private JButton corporationUserConnectionButton;
+	private JButton corporationOKButton;
+	
+	// user panels and layouts
+	private JPanel groupPanel;
+	private JPanel flowPanel;
 	private GroupLayout groupLayout;
 	private FlowLayout flowLayout;
+	
+	// corporation panels and layouts
+	private JPanel corporationGroupPanel;
+	private JPanel corporationFlowPanel;
+	private GroupLayout corporationGroupLayout;
+	private FlowLayout corporationFlowLayout;
+	
+	private JTabbedPane tabbedPane;
 	
 	public ReadCSV_GUI(String title, Application _app)
 	{
@@ -52,6 +76,15 @@ public class ReadCSV_GUI extends JFrame{
 		app = _app;
 		
 		// initialize the components
+		tabbedPane = new JTabbedPane();
+		initUserTab();
+		initCorporationTab();
+
+	}
+	
+	// Initalizes the user-adding tab of the GUI
+	public void initUserTab() {
+
 		usersLabel = new JLabel("Users CSV:");
 		usersLabel.setBorder(new EmptyBorder(0,0,0,10));
 		interestsLabel = new JLabel("Interests CSV:");
@@ -109,7 +142,7 @@ public class ReadCSV_GUI extends JFrame{
 		
 		groupLayout.setVerticalGroup(vGroup);
 		
-		// read and process the file when clicking the 'OK' button
+		// read and process the files when clicking the 'OK' button
 		okButton.addActionListener (new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -120,6 +153,7 @@ public class ReadCSV_GUI extends JFrame{
 				if (!usersFile.exists())
 				{
 					showFileNotFoundError("\"" + usersTextField.getText() + "\"");
+					return;
 				}
 				
 				File interestsFile = new File(interestsTextField.getText());
@@ -127,6 +161,7 @@ public class ReadCSV_GUI extends JFrame{
 				if (!interestsFile.exists())
 				{
 					showFileNotFoundError("\"" + interestsTextField.getText() + "\"");
+					return;
 				}
 				
 				File availabilitiesFile = new File(availabilitiesTextField.getText());
@@ -134,6 +169,7 @@ public class ReadCSV_GUI extends JFrame{
 				if (!availabilitiesFile.exists())
 				{
 					showFileNotFoundError("\"" + availabilitiesTextField.getText() + "\"");
+					return;
 				}
 				
 				try 
@@ -144,7 +180,97 @@ public class ReadCSV_GUI extends JFrame{
 				{
 					e1.printStackTrace();
 				}
-				dispose();
+				usersTextField.setText("");
+				interestsTextField.setText("");
+				availabilitiesTextField.setText("");
+			}
+		});
+	}
+	
+	// Initalizes the corporation-adding tab of the GUI
+	public void initCorporationTab() {
+		corporationsLabel = new JLabel("Corporations CSV:");
+		corporationsLabel.setBorder(new EmptyBorder(0,0,0,10));
+		corporationUserConnectionLabel = new JLabel("Corporation-user CSV:");
+		corporationUserConnectionLabel.setBorder(new EmptyBorder(0,0,0,10));
+		
+		corporationsTextField = new JTextField(50);
+		corporationUserConnectionTextField = new JTextField(50);
+		
+		corporationsButton = new JButton("Browse...");
+		corporationUserConnectionButton = new JButton("Browse...");
+		
+		corporationsButton.addActionListener(new CSVFileBrowser(corporationsTextField));
+		corporationUserConnectionButton.addActionListener(new CSVFileBrowser(corporationUserConnectionTextField));
+		
+		corporationOKButton = new JButton("OK");
+		corporationOKButton.setMargin(new Insets(10, 10, 10, 10));
+		
+		corporationGroupPanel = new JPanel();
+		corporationGroupLayout = new GroupLayout(corporationGroupPanel);
+		corporationGroupPanel.setLayout(corporationGroupLayout);
+		corporationGroupLayout.setAutoCreateContainerGaps(true);
+		
+		corporationFlowPanel = new JPanel();
+		corporationFlowLayout = new FlowLayout(FlowLayout.CENTER);
+		corporationFlowPanel.setLayout(corporationFlowLayout);
+		corporationFlowPanel.add(corporationOKButton);
+		
+		corporationGroupPanel.add(corporationFlowPanel);
+		
+		// configure the layouts
+		GroupLayout.SequentialGroup hGroup = corporationGroupLayout.createSequentialGroup();
+		hGroup.addGroup(corporationGroupLayout.createParallelGroup().
+		            addComponent(corporationsLabel).addComponent(corporationUserConnectionLabel));
+		hGroup.addGroup(corporationGroupLayout.createParallelGroup().
+					addComponent(corporationFlowPanel).addComponent(corporationsTextField).addComponent(corporationUserConnectionTextField));
+		hGroup.addGroup(corporationGroupLayout.createParallelGroup().
+					addComponent(corporationsButton).addComponent(corporationUserConnectionButton));
+	            
+		corporationGroupLayout.setHorizontalGroup(hGroup);
+		
+		GroupLayout.SequentialGroup vGroup = corporationGroupLayout.createSequentialGroup();
+		vGroup.addGroup(corporationGroupLayout.createParallelGroup(Alignment.BASELINE).
+		            addComponent(corporationsLabel).addComponent(corporationsTextField).addComponent(corporationsButton));
+		vGroup.addGroup(corporationGroupLayout.createParallelGroup(Alignment.BASELINE).
+	            addComponent(corporationUserConnectionLabel).addComponent(corporationUserConnectionTextField).addComponent(corporationUserConnectionButton));
+		vGroup.addGroup(corporationGroupLayout.createParallelGroup(Alignment.BASELINE).
+		            addComponent(corporationFlowPanel));
+		
+		corporationGroupLayout.setVerticalGroup(vGroup);
+		
+		// read and process the files when clicking the 'OK' button
+		corporationOKButton.addActionListener (new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				File corporationsFile = new File(corporationsTextField.getText());
+				
+				// Check if every file is valid
+				if (!corporationsFile.exists())
+				{
+					showFileNotFoundError("\"" + corporationsTextField.getText() + "\"");
+					return;
+				}
+				
+				File corporationUserConnectionFile = new File(corporationUserConnectionTextField.getText());
+				
+				if (!corporationUserConnectionFile.exists())
+				{
+					showFileNotFoundError("\"" + corporationUserConnectionTextField.getText() + "\"");
+					return;
+				}
+				
+				try 
+				{
+					// TODO: call the application's relevant function similiar to the user tab's OK button
+				} 
+				catch (Exception e1) 
+				{
+					e1.printStackTrace();
+				}
+				corporationsTextField.setText("");
+				corporationUserConnectionTextField.setText("");
 			}
 		});
 	}
@@ -157,7 +283,11 @@ public class ReadCSV_GUI extends JFrame{
 	// Packs the frame and sets it visible
 	public void start() {
 
-		add(groupPanel);
+		getContentPane().add(tabbedPane);
+		
+		tabbedPane.add("Users", groupPanel);
+		tabbedPane.add("Corporations", corporationGroupPanel);
+		
     	setResizable(false);
     	
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
