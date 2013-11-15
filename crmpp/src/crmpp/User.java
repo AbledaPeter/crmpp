d1c8b7f3bc40c607e7141091a1026bc07dd8c42b
Ableda Péter <abledapeter@gmail.com>
2013-11-15 13:27:29 +0100

    delete triial comments

--- crmpp/src/crmpp/User.java
+++ crmpp/src/crmpp/User.java
@@ -1,9 +1,5 @@
 package crmpp;
 
-/**
- *
- * @author Devids
- */
 public class User {
     
     private int id;
@@ -29,114 +25,66 @@ public class User {
     public User() {
     }
 
-    /**
-     * @return the id
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the firstName
-     */
     public String getFirstName() {
         return firstName;
     }
 
-    /**
-     * @param firstName the firstName to set
-     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    /**
-     * @return the lastName
-     */
     public String getLastName() {
         return lastName;
     }
 
-    /**
-     * @param lastName the lastName to set
-     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    /**
-     * @return the gender
-     */
     public String getGender() {
         return gender;
     }
 
-    /**
-     * @param gender the gender to set
-     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
-    /**
-     * @return the age
-     */
     public int getAge() {
         return age;
     }
 
-    /**
-     * @param age the age to set
-     */
     public void setAge(int age) {
         this.age = age;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the address
-     */
     public String getAddress() {
         return address;
     }
 
-    /**
-     * @param address the address to set
-     */
     public void setAddress(String address) {
         this.address = address;
     }
 
-    /**
-     * @return the sport
-     */
     public String getSport() {
         return sport;
     }
 
-    /**
-     * @param sport the sport to set
-     */
     public void setSport(String sport) {
         this.sport = sport;
     }
